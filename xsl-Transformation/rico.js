91df5fab3a76e4ce7712a8c6e7fb0c866ea4a331
dbkost1582 <dbkost1582@gmail.com>
2025-05-20 10:27:18 +0200

    Delete xsl-Transformation directory
    second try

--- xsl-Transformation/rico.js
+++
@@ -1,9 +0,0 @@
-/*! Display RIC-O special Parameter */
-function ricOn() {
-	var x = document.getElementById("addonRIC");
-	x.style.display = "block";
-}
-function ricOff() {
-	var x = document.getElementById("addonRIC");
-	x.style.display = "none";
-}
